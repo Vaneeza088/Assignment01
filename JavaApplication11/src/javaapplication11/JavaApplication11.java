01485ed800f3fc16b23145b9ae818b716ea17ad8
vaneeza <vaneeza@vaneezas-air.fios-router.home>
2021-02-17 21:30:26 -0500

    Print even numbers from 0 to 100

---
+++ JavaApplication11/src/javaapplication11/JavaApplication11.java
@@ -0,0 +1,16 @@
+package javaapplication11;
+
+/**
+ *
+ * @author vaneeza
+ */
+public class JavaApplication11 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i<=100; i++) //running loop from i = 0 to 100, i.e to cover all elements from 0 to 100
+            if (i %2 == 0){ //if the number is even then print it 
+        System.out.println(i);
+    }
+    
+}
+}
